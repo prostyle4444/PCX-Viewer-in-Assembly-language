223df7074cccb20f3edc9968ad4b2ed496b2c517
prostyle4444 <pranav.vidya@gmail.com>
2014-05-27 12:55:14 +0530

    Create pcx.asm

---
+++ pcx.asm
@@ -0,0 +1,19 @@
+use16
+org 100h
+
+mov al, 13h
+int 10h
+mov di, 3200
+mov si, image+80h
+call Draw_PCX
+mov ah, 00h
+int 16h
+mov ax, 3
+mov bx, 0
+int 10h
+ret
+
+image:
+file 'house.pcx' ; you can change it to whatever you want over here
+
+include 'pcx.inc'
